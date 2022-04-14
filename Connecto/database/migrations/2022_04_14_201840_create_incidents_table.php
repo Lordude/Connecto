fc6b73a9b1b313f509b0f4745ff1ad3e5ada36fa
Geoffroy Lambert <geoffroy.lambert042@outlook.com>
2022-04-14 16:45:35 -0400

    debut de migration DB pour incidents
    reste juste les FK a implementer !

---
+++ Connecto/database/migrations/2022_04_14_201840_create_incidents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->id();
+            $table->date_debut();
+            $table->date_fin();
+            $table->commentaire();
+            $table->id_user();
+            $table->etat_incident_id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+};
---
+++ Connecto/database/migrations/2022_04_14_201918_create_incident_service_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incident_service', function (Blueprint $table) {
+            $table->incident_id();
+            $table->service_id();
+            $table->primary(['incident_id'], ['service_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incident_service');
+    }
+};
