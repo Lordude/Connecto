019f42cccf7af134d5cf842cbba427c0891bfc59
Annabelle Nicole <annabelle22003@hotmail.com>
2022-04-20 21:31:45 -0400

    Ajout des fonctions hasMany, belongsTo et use

--- Connecto/app/Models/Incident.php
+++ Connecto/app/Models/Incident.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Service;
+use App\Models\State;
 
 class Incident extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsToMany(State::class);
+    }
 }
--- Connecto/app/Models/State.php
+++ Connecto/app/Models/State.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Incident;
 
 class State extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    public function incident()
+    {
+        return $this->hasMany(Incident::class);
+    }
 }
