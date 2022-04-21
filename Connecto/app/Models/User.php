c4aa9d3d4eff3f26366e34a1ca014c6f49269957
Annabelle Nicole <annabelle22003@hotmail.com>
2022-04-21 16:12:20 -0400

    correction incident pour incidents

--- Connecto/app/Models/User.php
+++ Connecto/app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function incident()
+    public function incidents()
     {
         return $this->hasMany(Incident::class);
     }
