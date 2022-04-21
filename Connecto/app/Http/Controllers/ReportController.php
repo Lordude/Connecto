c4d6d0b4b0dd8f09f53ccdae2e3193b199353788
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-04-20 21:05:06 -0400

    report update

--- Connecto/app/Http/Controllers/ReportController.php
+++ Connecto/app/Http/Controllers/ReportController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Report;
 use App\Http\Controllers\Controller;
 
+
+
+
 class ReportController extends Controller
 {
     /**
--- Connecto/app/Models/Report.php
+++ Connecto/app/Models/Report.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+
+
 
 class Report extends Model
 {
     use HasFactory;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'report',
+        'date',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the product that owns the ProductOption.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Report::class);
+    }
 }
+    
