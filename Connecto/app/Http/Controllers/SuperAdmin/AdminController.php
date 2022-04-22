f9a91cc474b16a5d24ca6929c30449ffe032f4a8
Lordude <geoffroy.lambert042@outlook.com>
2022-04-22 10:35:26 -0400

    view et layout superadmin
    (juste un copy + paste du template admin pour l'instant)

---
+++ Connecto/app/Http/Controllers/SuperAdmin/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\SuperAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    //
+}
