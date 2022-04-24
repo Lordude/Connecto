98861e8340656489b36b0f702bd57ff090fb19d5
Annabelle Nicole <annabelle22003@hotmail.com>
2022-04-24 14:45:32 -0400

    formulaire créations incidents caché

--- Connecto/resources/views/admin/incidents/index.blade.php
+++ Connecto/resources/views/admin/incidents/index.blade.php
@@ -8,22 +8,26 @@
 
     <h2>Gestion des incidents</h2>
 
-    <button type="button" class="btn btn-warning">Créer un incident</button>
+    <button onClick="incidentForm()" type="button" class="btn btn-warning">Créer un incident</button>
     <div>
         {{-- quand on clic sur 'créer un incident' le formulaire ci-dessous apparait --}}
         <div class="row">
             <div class="col6 col-lg-6">
-                <form method="POST" action="{{ route('admin.incidents.store') }}">
+
+                <form id="hidden" method="POST" action="{{ route('admin.incidents.store') }}">
                     @csrf
 
                     <h3>Nouvel incident</h3>
-
-                    <div class="mb-3">
+                    {{-- {{$incidents->incidentFormClose()}} --}}
+                    <div class="mb-3 container">
                         <label for="services">Choisir les services affectés</label>
 
                         @foreach ($services as $service)
-                            <input type="checkbox" id="service" name="services[]" value="{{ $service->id }}"><label
-                                for="service">{{ $service->name }}</label>
+                            <ul class="list-group">
+                                <li class="list-group-item"><input class="form-check-input me-1" type="checkbox" id="service"
+                                        name="services[]" value="{{ $service->id }}"><label
+                                        for="service">{{ $service->name }}</label></li>
+                            </ul>
                         @endforeach
                         <hr />
                         <div class="mb-3">
@@ -55,44 +59,50 @@
     </div>
     {{-- vue des incidents en cours --}}
     <tbody>
+        <div>
+            <table class="table container-md">
+                <thead>
+                    <h2>Incidents</h2>
 
-        <table class="table">
-            <thead>
-                <h2>Incidents</h2>
+                    <th>Service affecté</th>
+                    <th>État</th>
+                    <th></th>
+                    <th>Description</th>
+                    <th>Date de début</th>
+                    <th>Administrateur</th>
+                    <th>Option</th>
+                </thead>
+                <tbody>
+                    @foreach ($incidents as $incident)
+                        @if ($incident->end_date == null)
+                            <tr>
+                                <td>
+                                    @foreach ($incident->services as $service)
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item">{{ $service->name }}</li>
+                                        </ul>
+                                    @endforeach
+                                </td>
+                                <td>{{ $service->get_service_state($service->id)->first()->name }}</td>
+                                <td><img src="{{ asset('image/') . $service->get_service_image($service->id) }}"
+                                        alt="icone" /></td>
+                                {{-- <td><img src="{{ asset('image/ {$service->get_service_image($service->id}' )}}" alt="icone"/></td> --}}
+                                <td>{{ $incident->commentary }}</td>
+                                <td>{{ $incident->start_date }}</td>
+                                <td>{{ $incident->adminIncident($incident->user_id)->first()->first_name }} {{ $incident->adminIncident($incident->user_id)->first()->last_name }} </td>
+                                <td>
+                                    <button type="button" class="btn btn-warning">
+                                        <a href="{{ route('admin.incidents.edit', ['incident' => $incident]) }}"
+                                            class="btn btn-link">Modifier</a>
+                                    </button>
+                                    {{-- en cliquant sur modifier, le formulaire va devenir éditable --}}
+                                </td>
 
-                <th>Service affecté</th>
-                <th>État</th>
-                <th>Description</th>
-                <th>Date de début</th>
-                <th>Date de fin</th>
-                <th>Option</th>
-            </thead>
-            <tbody>
-                @foreach ($incidents as $incident)
-                    @if ($incident->end_date == null)
-                        <tr>
-                            <td>
-                                @foreach ($incident->services as $service)
-                                    {{ $service->name }}
-                                @endforeach
-                            </td>
-                            <td>{{ $service->get_service_state($service->id)->first()->name }}</td>
-                            <td>{{ $incident->commentary }}</td>
-                            <td>{{ $incident->start_date }}</td>
-                            <td>{{ $incident->end_date }}</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">
-                                    <a href="{{ route('admin.incidents.edit', ['incident' => $incident]) }}"
-                                        class="btn btn-link">Modifier</a>
-                                </button>
-                                {{-- en cliquant sur modifier, le formulaire va devenir éditable --}}
-                            </td>
-
-                        </tr>
-            </tbody>
-            {{-- @else --}}
-            {{-- <p> Aucun services à afficher présentement </p> --}}
-            @endif
-            @endforeach
-        </table>
-    @endsection
+                            </tr>
+                </tbody>
+                {{-- @else --}}
+                {{-- <p> Aucun services à afficher présentement </p> --}}
+                @endif
+                @endforeach
+            </table>
+        @endsection
