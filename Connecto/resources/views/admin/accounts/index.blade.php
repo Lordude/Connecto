4dd36cfa720ac64c06f6ad57a9972509f51284eb
EtienneDeziel <enneiteleized@hotmail.com>
2022-04-24 09:17:27 -0400

    Ajout du Logo
    Un peu de css/bootstrap pour chiller avec le café ce matin.
    Ajout du logo et de quelques couleurs et mise en place.

--- Connecto/resources/views/admin/accounts/index.blade.php
+++ Connecto/resources/views/admin/accounts/index.blade.php
@@ -1,23 +1,19 @@
     @include('layouts.includes.head')
 
-
-    <div class="p-2 mb-2 bg-dark text-white text-end">Compte administrateur
-        <svg xmlns="http://www.w3.org/2000/svg" id="moe" width="16" height="16" fill="red" class="bi bi-person-circle" onclick="openForm()" viewBox="0 0 16 16">
+    <div class="p-2 mb-2 bg-dark text-white text-end fill">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" onclick="openForm()" viewBox="0 0 16 16">
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
         </svg>
+        Compte administrateur
     </div>
+    <div class=" p-4 mb-2 filll"></div>
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="">Connecto</a>
-        </div>
-    </nav>
-    
+    <img style="width: 200px" src="{{ asset('image/RectangleText.png') }}">
 
     @if (session('logfail'))
             <div class="alert alert-sucess">
-                {{session('logfail')}}
+               <p class="MessageSession">{{session('logfail')}}</p>
             </div>
     @endif
 
@@ -25,9 +21,6 @@
         @yield('content')
     </div> 
 
-
-
-        
     <div class="form-popup" id="myForm">
     <form action="{{ route('admin.accounts.index') }}" method="POST" class="form-container">
         @csrf
