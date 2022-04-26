6cc59f66039b4759cc64d25cc8be43f6cec3182f
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-04-25 20:51:41 -0400

    creation dossier page signalement
    coté admin voir les signalement

---
+++ Connecto/app/Http/Controllers/Admin/ReportServiceController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\Report;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReportRequest;
+
+
+class ReportService extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reports = Report::all();
+
+        return view('home.reports.index',
+         ['reports' => $reports]
+        );
+    }
+    public function show($id)
+    {
+        $reports = Report::findOrFail($id);
+
+        return view('home.reports.show', [
+            'report' => $reports,
+            'report_options' => $reports->report_options()->get()
+        ]);
+    }
+
+ /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $reports = new Report;
+
+        return view('home.reports.create', ['report' => $reports]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $report = Report::create($request->validate([
+            'report_id' => ['required'],
+            'service_id' => ['required'],
+            'detail' => ['required'],
+            
+        ]));
+
+       
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $reports = Report::findOrFail($id);
+
+        return view('home.reports.edit', ['Reports' => $reports]);
+    }
+
+
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Report::findOrFail($id)->delete();
+
+        return redirect()->route('home.reports.index');
+    }
+
+   
+    
+}
