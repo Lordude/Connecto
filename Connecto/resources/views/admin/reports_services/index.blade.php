b605dff44c5f1a95369aac7ce49237ba5ba57cd1
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-04-28 15:34:20 -0400

    Modal report_service

--- Connecto/resources/views/admin/reports_services/index.blade.php
+++ Connecto/resources/views/admin/reports_services/index.blade.php
@@ -32,6 +32,32 @@
                     @endforeach
                 </tbody>
             </table>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="report_id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+
         @else
             <p> Aucun produit à afficher pour le moment ! </p>
         @endif
