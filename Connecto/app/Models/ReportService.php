2933d38a743640da789c4990f1b8d0c5f06c53e8
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-01 21:25:35 -0400

    signalement 24 heure
    Il se mutiplie par 7 fois la reponse car il a eu 7 signalement. enlever le array. Bref pas fini mais formule ok

--- Connecto/app/Models/ReportService.php
+++ Connecto/app/Models/ReportService.php
@@ -1,9 +1,4 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+@@ -7,11 +7,14 @@
 use Illuminate\Support\Facades\DB;
 use App\Models\Service;
 use App\Models\Report;
@@ -18,43 +13,7 @@ class ReportService extends Model
 {
     use HasFactory;
 
-    
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'report_id',
-        'service_id',
-        'created_at',
-        'updated_at',
-        
-    ];
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'created_at' => 'date',
-    ];
-
-    
-    public function services()
-    {
-        return $this->belongsToMany(Service::class);
-
-    }
-    public function reports()
-    {
-        return $this->hasOne(Report::class);
-    
-    }
-
-    public function reports_services()
-    {
-        return $this->hasOne(ReportService::class);
+@ -55,6 +58,12 @@ public function reports_services()
     }
 
 
@@ -67,33 +26,25 @@ class ReportService extends Model
     public function get_report_id($report_id)
     {
         $result = DB::table('reports')
-        ->join('reports', 'reports_services.report_id', '=', 'report.id')
-        ->select('reports')
-        ->where('report_id', '=', $report_id)
-        ->get();
+@ -79,17 +88,13 @@ public function get_service_id($service_id)
 
         if($result->count()){
             return $result;
+      
+        }
+
         }
     }
-
-    
-    public function get_service_id($service_id)
+    public function get_report_sub_hours($report_id)
     {
-            $result = DB::table('services')
-                        ->join('report_service', 'services.id', '=', 'report_service.service_id')
-                        ->select('reports.detail')
-                        ->where('service_id', '=', $service_id)
-                        ->get();
-
-        if($result->count()){
-            return $result;
-
-        }
-    }
+        $reports = Report::where('report_id', 'active')
+        ->where( 'created_at', '>', Carbon::now()->subHours(24))
+        ->get();
     
 
 
+    }
+}
 
 
 
