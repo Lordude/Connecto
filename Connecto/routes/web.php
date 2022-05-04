d7b3e0ee59a9e2af481ff480a775a9675b29b4f7
Annabelle Nicole <annabelle22003@hotmail.com>
2022-05-04 11:58:51 -0400

    Update web.php

--- Connecto/routes/web.php
+++ Connecto/routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\SuperAdmin\UserController;
+use App\Http\Controllers\LoginController;
 
 /*
-|---use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
