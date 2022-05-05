9a2eff204ca8fec0cfa46b732cf48641d84558bf
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-05 16:40:42 -0400

    bouton modal
    bouton modal admin report

--- Connecto/resources/views/admin/reports_services/index.blade.php
+++ Connecto/resources/views/admin/reports_services/index.blade.php
@@ -37,18 +37,41 @@
                                 </td>
 
                                 {{-- <td>{{ $service->get_service_state($service->id)->first()->name }}</td> --}}
-                               
+
 
                                 {{-- <td>{{ $report->get_report_detail($report->id)->first()->detail }}</td> --}}
                                 <td>{{ $report->email }}</td>
-                                <td>{{ $report->detail }}</td>
-                                <td>{{ $report->frequent_issue_id }}</td>
-                             
-                               
-                               
-                               
-     
+                                
 
+              
+                               
+                                
+                                <td><!-- Button trigger modal -->
+                                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signalement_{{$report->id}}">
+                                          Détail du signalement
+                                        </button>
+                                        
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="signalement_{{$report->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                          <div class="modal-dialog">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                <h5 class="Commentaire" id="staticBackdropLabel">Commentaire</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                              </div>
+                                              <div class="modal-body">
+                                                <div class="">{{$report->detail}}</div>
+                                              </div>
+                                              
+                                              <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Understood</button>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div> </td>
+                                <td>{{ $report->frequent_issue_id }}</td>
+ 
                             </tr>
                 </tbody>
                 {{-- @else --}}
@@ -79,27 +102,3 @@
 
 
 
-@endsection
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        détail du signalement
-      </button>
-      
-      <!-- Modal -->
-      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Understood</button>
-            </div>
-          </div>
-        </div>
-      </div>
