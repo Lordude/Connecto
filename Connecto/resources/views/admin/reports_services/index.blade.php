e47f1645ccf354fa9e83003515b3cbed63fc5a19
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-12 16:06:20 -0400

    modal pour Type problème

--- Connecto/resources/views/admin/reports_services/index.blade.php
+++ Connecto/resources/views/admin/reports_services/index.blade.php
@@ -68,7 +68,49 @@
                                 </td>
                                 <td>
 
-                                    {{ $report->frequent_issue->problem}}
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-warning text-white" data-bs-toggle="modal"
+    data-bs-target="#signalement__{{ $report->id }}">
+    Type
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="signalement__{{ $report->id }}"
+    data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdrop" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="Type de problème" id="staticBackdrop">Type de problème</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class=""> {{ $report->frequent_issue->problem}}</div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary"
+                    data-bs-dismiss="modal">Fermer</button>
+         
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                             
 
                                 </td>
                                 <td>{{ $report->created_at }}</td>
