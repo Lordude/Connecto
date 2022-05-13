bc39e0e53ef85e606e32422479199ecf0332c3d9
Annabelle Nicole <annabelle22003@hotmail.com>
2022-05-12 21:14:07 -0400

    Update HomeController.php

--- Connecto/app/Http/Controllers/HomeController.php
+++ Connecto/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Service;
+use App\Models\Incident;
 use Carbon\Carbon;
 
 class HomeController extends Controller
@@ -11,8 +12,9 @@ class HomeController extends Controller
     public function index()
     {
         $services = Service::all();
+        $incidents = Incident::all();
 
-        return view('home.index', ['services' => $services]);
+        return view('home.index', ['services' => $services, 'incidents' => $incidents]);
     }
 
 }
