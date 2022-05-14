8cf251be4b378d270564367eecbc723780e210a2
Annabelle Nicole <annabelle22003@hotmail.com>
2022-05-14 17:47:32 -0400

    css création service

--- Connecto/resources/views/admin/services/edit.blade.php
+++ Connecto/resources/views/admin/services/edit.blade.php
@@ -3,26 +3,27 @@
 @section('title', 'Ajouter un service')
 
 @section('content')
-<div class="col-9">
-    <h1>Modifier un service</h1>
-    <hr>
+    <div class="col-9">
+        <h1>Modifier un service</h1>
+        <hr>
 
-    <form method="POST" action="{{ route('admin.services.update', ['service' => $service]) }}">
-        @csrf
-        @method('PUT')
+        <form method="POST" action="{{ route('admin.services.update', ['service' => $service]) }}">
+            @csrf
+            @method('PUT')
 
-        <div class="editFormStatus flexEditForm">
-            <label for="name" class="form-label">Nom du service</label>
-            <input id="name" name="name" type="text" value="{{ $service->name }}" class="form-control inputServiceEdit">
-        </div>
-<br/>
-<div
-<div class="editFormButton">
-    <input type="submit" value="Enregistrer" class="btn btn-warning text-white">
-    <a href="{{ route('admin.services.index') }}" class="btn text-danger"> Retour </a>
-</div>
-    </form>
+            <div class="editFormStatus flexEditForm">
+                <label for="name" class="form-label">Nom du service</label>
+                <input id="name" name="name" type="text" value="{{ $service->name }}"
+                    class="form-control inputServiceEdit">
+            </div>
+            <br />
+            <div>
+                <div class="editFormButton">
+                    <input type="submit" value="Enregistrer" class="btn btn-warning text-white">
+                    <a href="{{ route('admin.services.index') }}" class="btn text-danger"> Retour </a>
+                </div>
+        </form>
     </div>
-</div>
+    </div>
 
 @endsection
--- Connecto/resources/views/admin/services/index.blade.php
+++ Connecto/resources/views/admin/services/index.blade.php
@@ -3,17 +3,18 @@
 @section('title', 'Accueil')
 
 @section('content')
-<div class="col-9">
-    <h1>Service</h1>
-    <hr>
-<a id="BtnCreateServ" class="btn btn-warning text-white margeLeftHautPage" href="{{ route('admin.services.create') }}">Ajouter un service </a>
+    <div class="col-9">
+        <h1>Service</h1>
+        <hr>
+        <a id="BtnCreateServ" class="btn btn-warning text-white margeLeftHautPage"
+            href="{{ route('admin.services.create') }}">Ajouter un service </a>
 
-@if (session('logsuccess'))
-        <p class="MessageSession">{{session('logsuccess')}}</p>
-@endif
-<p class="MessageSession"> {{ session('NotSuper') }} </p>
+        @if (session('logsuccess'))
+            <p class="MessageSession">{{ session('logsuccess') }}</p>
+        @endif
+        <p class="MessageSession"> {{ session('NotSuper') }} </p>
 
-        @if($services->count() > 0)
+        @if ($services->count() > 0)
             <table class="table">
                 <thead>
                     <th>Nom du service</th>
@@ -22,15 +23,24 @@
                 <tbody>
                     @foreach ($services as $service)
                         <tr>
-                            <td class="align-middle"> {{$service->name}} </td>
-                            <td> <img width="42px" height="42px" src="../image/{{$service->get_service_image($service->id)->first()->image;}}" alt="Icone de l\'etat du service {{$service->get_service_image($service->id)->first()->image;}}"></td>
-                            <td><button type="button" class="btn btn-warning"><a class="btn btn-link yellowButton" href="{{route('admin.services.edit', ['service' => $service]) }}">Modifier </a></button> </td>
-                            <td><form method="POST" action="{{ route('admin.services.destroy', ['service' => $service]) }}">
-                                @csrf
-                                @method('DELETE')
+                            <td class="align-middle"> {{ $service->name }} </td>
+                            <td> <img width="42px" height="42px"
+                                    src="../image/{{ $service->get_service_image($service->id)->first()->image }}"
+                                    alt="Icone de l\'etat du service {{ $service->get_service_image($service->id)->first()->image }}">
+                            </td>
+                            <td><button type="button" class="btn btn-warning"><a class="btn btn-link yellowButton"
+                                        href="{{ route('admin.services.edit', ['service' => $service]) }}">Modifier
+                                    </a></button> </td>
+                            <td>
+                                <form method="POST"
+                                    action="{{ route('admin.services.destroy', ['service' => $service]) }}">
+                                    @csrf
+                                    @method('DELETE')
 
-                                <input type="submit" value="Supprimer" class="btn btn-danger text-white" onclick="return confirm('Are you sure?')" />
-                              </form></td>
+                                    <input type="submit" value="Supprimer" class="btn btn-danger text-white"
+                                        onclick="return confirm('Êtes-vous certain de vouloir supprimer ce service?')" />
+                                </form>
+                            </td>
 
                         </tr>
                     @endforeach
@@ -40,5 +50,5 @@
             <p> Aucun produit à afficher pour le moment ! </p>
         @endif
     </div>
-</div>
+    </div>
 @endsection
