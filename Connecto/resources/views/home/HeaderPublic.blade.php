3bea7f4dbcf5fcd8a1a6761d8d197efc55fd3b44
EtienneDeziel <enneiteleized@hotmail.com>
2022-05-14 21:47:44 -0400

    Test Header
    Test Header

--- Connecto/resources/views/home/HeaderPublic.blade.php
+++ Connecto/resources/views/home/HeaderPublic.blade.php
@@ -1,29 +1,45 @@
-    <div class="p-2 mb-2 bg-dark text-white text-end fill">
-    @if(session()->has('emailUser'))
-    <a href="{{ route('login.index') }}">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
-        </a>
-        Déconnexion
-    @else
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" onclick="openForm()" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
-        Compte administrateur
-    @endif
-    </div>
-    <div class=" p-4 mb-2 filll"></div>
-<div class="divTauxDisponibilite">
-    <a href="https://status.nest.com/"><img class="LogoHeader" src="{{ asset('image/RectangleText.png') }}"></a>
-    <p class="tauxDisponibilite"> Les services sont opérationnels
-        <?php
-        echo App\Models\Incident::get_Uptime() . '% du temps';
-        ?>
-    </p>
-</div>
+<header>
+    <div class="container-fluid"> <!--CONTAINER IN -->
+        <div class="row bg-dark text-white text-end fill"> <!--ROW-1 IN -->
+            @if(session()->has('emailUser'))
+            <div class="col-md-4 offset-md-8">
+                <a href="{{ route('login.index') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                    Déconnexion
+                </a>
+            </div>
+            @else
+            <div class="col-md-4 offset-md-8">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" onclick="openForm()" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                </svg>
+                Compte administrateur
+            </div>
+            @endif
+        </div> <!--ROW-1 OUT -->
+
+        <div class="row filll"></div> <!--ROW-2 IN OUT -->
+
+        <div class="row"> <!--ROW-3 IN -->
+            <div class="col-md-4">
+                <div style="width: 50%;">
+                <a href="https://status.nest.com/"><img class="img-fluid" src="{{ asset('image/RectangleText.png') }}"></a>
+                </div>
+            </div>
+            <div class="col-md-4 offset-md-4">
+                <p class="tauxDisponibilite"> Les services sont opérationnels
+                    <?php echo App\Models\Incident::get_Uptime() . '% du temps';?>
+                </p>
+            </div>
+
+        </div><!--ROW-3 OUT -->
+
+    </div> <!--CONTAINER OUT -->
+</header>
     @if (session ('AccessDenided'))
     <div class="alert alert-danger alert-dismissible fade show MessageSession ">
         {{ session('AccessDenided') }}
