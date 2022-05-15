d07a4b48eb44ee10a7de6dfb1c22ce101a7a5c20
EtienneDeziel <enneiteleized@hotmail.com>
2022-05-15 16:37:55 -0400

    text decoration
    text decoration none

--- Connecto/resources/views/layouts/admin/headerAdmin.blade.php
+++ Connecto/resources/views/layouts/admin/headerAdmin.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid"> <!--CONTAINER IN -->
         <div class="row bg-dark text-white text-end fill"> <!--ROW-1 IN -->
             <div class="col-md-4 offset-md-8">
-                <a href="{{ route('login.index') }}">
+                <a class="textDeconexion" href="{{ route('login.index') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" onclick="" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
