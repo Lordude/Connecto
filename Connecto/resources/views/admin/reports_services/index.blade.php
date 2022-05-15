869d126a98fa21c98a5dc5cb1c0e42649bd354cd
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-15 02:29:57 -0400

    update css signalement admin

--- Connecto/resources/views/admin/reports_services/index.blade.php
+++ Connecto/resources/views/admin/reports_services/index.blade.php
@@ -3,54 +3,62 @@
 @section('title', 'Signalement')
 
 @section('content')
+
+
 <div class="col-9">
 
-
-    
-        <h1>Signalement</h1>
-        <hr />
-        <div>
-            <form>
-        <div class="tableReport">
-            <thead>
-<h5>Service affecté</h5>
+    <h1>Signalement</h1>
 
 
-<div class="mb-3 container p-2">
-
-<div class="reportFForm">
-
-    @foreach ($reports as $report)
-                        
- @foreach ($report->services as $service)
-
-<ul class="list-group-item">{{ $service->name }}
-
-        
-<div class="accordion" id="accordion">
-    <div class="accordion-item">
-<h2 class="accordion-header" id="headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-Voir plus
-        </button>
-</h2>
-<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-    <br/>
-        <div class=""><strong>Commentaire: </strong> <br/>{{ $report->detail }}</div>
-        <div> <a href="mailto:<strong>Courriel de l'expéditeur(trice)</strong> <br/> {{ $report->email }}">{{ $report->email }}</a></div>
-        <div class=""><strong>Date: </strong> <br/>{{ $report->date }}</div>
-        <div class=""><strong>Problème courant: </strong> <br/>{{ $report->frequent_issue->problem}}</div>
-
-</form>
-                        </div>
-                     </div>
-                </div>
-            </div>         
+    <hr />
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
+    @endif
+    
+            <div class="tableIncident">
+                <h3 class="incidentTitle">Service affecté</h3>
+                <hr />
+                <div class="mb-3 container p-2">
+                  
+                    <div class="incidentForm">
 
-    </div>
-</ul>
-    @endforeach
-    @endforeach
-@endsection
+                        @foreach ($reports as $report)
+                        
+                        @foreach ($report->services as $service)
+                       
+                        <ul class="list-group incidentServicesForm">
+                       <div class="accordion" id="accordion">
+                           <div class="reportForm3">
+                       <h2 class="accordion-header" id="headingOne">
+                               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                       Voir plus
+                               </button>
+                               
+                               <ul class="list-group reportServicesForm">
+                       </h2>
+                       <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                           <br/>
+                           <div class="mb-3">
+                            
+                               <div class="pourtour"><strong>Commentaire: </strong> <br/>{{ $report->detail }}</div>
+                               <div  class="pourtour"><a href="mailto:<strong>Courriel de l'expéditeur(trice)</strong> <br/> {{ $report->email }}">{{ $report->email }}</a></div>
+                               <div class="pourtour"><strong>Date: </strong> <br/>{{ $report->date }}</div>
+                               <div class="pourtour"><strong>Problème courant: </strong> <br/>{{ $report->frequent_issue->problem}}</div>
+                          
 
+                        
+                       </div>
+                    </div>
+                </div>
+                                </ul>
+                             
+                          
+                            @endforeach
+                            @endforeach
+                        @endsection
