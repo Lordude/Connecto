d0711ae47c78a983be27634848cf427184dba2e4
Annabelle Nicole <annabelle22003@hotmail.com>
2022-05-16 09:57:26 -0400

    css tableau accueil

--- Connecto/resources/views/home/index.blade.php
+++ Connecto/resources/views/home/index.blade.php
@@ -1,4 +1,4 @@
-@extends(session()->has('emailUser') ? 'layouts.admin.app' : 'layouts.app' )
+@extends(session()->has('emailUser') ? 'layouts.admin.app' : 'layouts.app')
 
 @section('title', 'Bienvenu')
 
@@ -6,10 +6,11 @@
     <div class="col-md-9">
         <div class="row" style="margin:1em;">
             <div class="col-md-8">
-                <h1>Services Connecto</h1>
+                <h1 class=" titreBoutonAccueilRes">Services Connecto</h1>
             </div>
-            <div class="col-md-4">
-                <button type="button" class="btn btn-warning text-white margeLeftHautPage"><a class="yellowButton" href="{{ route('home.reports.create') }}"> Signaler une panne</a>
+            <div class="col-md-4 titreBoutonAccueilRes">
+                <button type="button" class="btn btn-warning text-white margeLeftHautPage"><a class="yellowButton"
+                        href="{{ route('home.reports.create') }}"> Signaler une panne</a>
                 </button>
             </div>
         </div>
@@ -18,29 +19,30 @@
                 opérationnels </p>
         @endif
         @if ($services->count() > 0)
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <th>Services</th>
-                    <th></th>
-                    <th>Statut</th>
-                    <th>Description</th>
-                </thead>
-                <tbody>
-                    @foreach ($services as $service)
-                        <tr>
-                            <td>{{ $service->name }} </td>
-                            <td>{{ $service->incident }}</td>
-                            <td> <img width="42px" height="42px"
-                                    src="./image/{{ $service->get_service_image($service->id)->first()->image }}"
-                                    alt="Icone de l\'etat du service {{ $service->get_service_image($service->id)->first()->image }}">{{ $service->get_service_state($service->id)->first()->name }}
-                            </td>
-                            <td> {{ $service->get_service_description($service->id)->first()->description }} </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="enteteTableauResponsive">
+                        <th>Services</th>
+                        <th></th>
+                        <th>Statut</th>
+                        <th>Description</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($services as $service)
+                            <tr>
+                                <td class="tableauAccueil serviceNameAccueilResponsive">{{ $service->name }} </td>
+                                <td class="tableauAccueil iconesServicesAccueil">{{ $service->incident }}</td>
+                                <td class="tableauAccueil iconesServicesAccueil"> <img width="42px" height="42px"
+                                        src="./image/{{ $service->get_service_image($service->id)->first()->image }}"
+                                        alt="Icone de l\'etat du service {{ $service->get_service_image($service->id)->first()->image }}">{{ $service->get_service_state($service->id)->first()->name }}
+                                </td>
+                                <td class="tableauAccueil descriptionTableauAccueil">
+                                    {{ $service->get_service_description($service->id)->first()->description }} </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         @endif
     </div>
 @endsection
