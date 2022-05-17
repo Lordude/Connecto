aa271101a363712df3366a169bc1330db54c71b6
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-17 08:34:19 -0400

    footer

--- Connecto/resources/views/layouts/footer.php
+++ Connecto/resources/views/layouts/footer.php
@@ -1,32 +1,4 @@
-<footerLigne></footerLigne>
+<footer>&copy; Connecto 2022. développeurs: Algoritmus.</footer>
   <!-- Footer-->
   
-  <footer class="footer bg-light">
-            <div class="container">
-                <div class="row">
-                    
-                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-                        <ul class="list-inline mb-2">
-                        <div class="col-md-4">
-                <div style="width: 75%;">
-                            <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!"><img class="img-fluid" src="image/RectangleText.png"></a></li>
-                            </div>
-                           
-                        </ul>
-                        <p class="text-muted small mb-4 mb-lg-0">&copy; Connecto 2022. développeurs: Algoritmus.</p>
-                    </div>
-                   
-                        
-                       
-                </div>
-            </div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-   
- 
-   
+  
