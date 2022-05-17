6d21b2005e32862ac6e2c9863b0a4669231e03ef
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-16 21:08:13 -0400

    update footer
    css footer

--- Connecto/resources/views/layouts/footer.php
+++ Connecto/resources/views/layouts/footer.php
@@ -1,1 +1,22 @@
-<footer>@2022 Algorithmus</footer>
+<footerLigne></footerLigne>
+  <!-- Footer-->
+  <div class="row filll"></div> <!--ROW-2 IN OUT -->
+    <div class="col-md-4 offset-md-4">
+    <div style="width: 50%;">
+        <p class="list-inline-item"><a href="#!"><img class="img-fluid" src="image/RectangleText.png"></a>
+        </p>
+    </div>
+<div>
+         <p class="text-muted small mb-4 mb-lg-0">&copy; Connecto 2022. développeurs: Algoritmus.</p>
+            </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+   
+ 
+   
