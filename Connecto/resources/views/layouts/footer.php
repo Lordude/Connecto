989a28e629b32ccbd15f54ad3f9205c833c23ff5
Valérie <97419749+valcarbo@users.noreply.github.com>
2022-05-16 21:24:13 -0400

    css footer

--- Connecto/resources/views/layouts/footer.php
+++ Connecto/resources/views/layouts/footer.php
@@ -1,14 +1,24 @@
 <footerLigne></footerLigne>
   <!-- Footer-->
-  <div class="row filll"></div> <!--ROW-2 IN OUT -->
-    <div class="col-md-4 offset-md-4">
-    <div style="width: 50%;">
-        <p class="list-inline-item"><a href="#!"><img class="img-fluid" src="image/RectangleText.png"></a>
-        </p>
-    </div>
-<div>
-         <p class="text-muted small mb-4 mb-lg-0">&copy; Connecto 2022. développeurs: Algoritmus.</p>
-            </div>
+  
+  <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                        <div class="col-md-4">
+                <div style="width: 75%;">
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!"><img class="img-fluid" src="image/RectangleText.png"></a></li>
+                            </div>
+                           
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Connecto 2022. développeurs: Algoritmus.</p>
+                    </div>
+                   
+                        
+                       
                 </div>
             </div>
         </footer>
